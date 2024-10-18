1230628611bdafa40a385fa10aab423a976fa19e
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-18 14:26:22 +0200

    generate components and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent)
+    },
+    {
+        path: "imprint",
+        loadComponent: () => import('./pages/imprint/imprint.component').then((m) => m.ImprintComponent)
+    },
+    {
+        path: "not-found",
+        loadComponent: () => import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent)
+    },
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    },
+    {
+        path: '**', redirectTo: 'not-found', pathMatch: 'full'
+    }
+
+];
