476c052ba5498fbe8eb79ad18aa0b5bd0b4ce178
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-19 09:36:03 +0200

    header: add animation for language toggle, add hover dot for links hero: change animation for hover dot

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  language: 'en' | 'de' = 'en';
+
+  toggleLanguage(lg: 'en' | 'de') {
+    this.language = lg; 
+  }
 }
