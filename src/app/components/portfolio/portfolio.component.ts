2ec3e82290a0a70b1dee4c6cb6176045b3c13a16
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-19 15:00:50 +0200

    portfolio: style component

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { fadeIn } from "../../shared/animations";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  animations: [fadeIn],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  showProjectImage: 'dabubble' | 'join' | 'epl' | '' = '';
+  showImage = false;
+
+  onMouseOver(project: 'dabubble' | 'join' | 'epl' | ''){
+    this.showProjectImage = project;
+    this.showImage = true;
+  }
+
+  onMouseLeave(){
+    this.showProjectImage = '';
+    this.showImage = false;
+  }
 
 }
