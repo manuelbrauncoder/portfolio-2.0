65451e0ea4cecd698e18988907e9d3b886a9ba66
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-19 10:43:14 +0200

    about-me: style section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../../components/hero/hero.component";
+import { AboutMeComponent } from "../../components/about-me/about-me.component";
+import { TechnologiesComponent } from "../../components/technologies/technologies.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
