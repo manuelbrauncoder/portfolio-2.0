5abd61a73f734a1f0652de1afd46488803dbef72
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-19 12:06:08 +0200

    portfolio: start with section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../../components/hero/hero.component";
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
 import { TechnologiesComponent } from "../../components/technologies/technologies.component";
+import { PortfolioComponent } from "../../components/portfolio/portfolio.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent],
+  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent, PortfolioComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
