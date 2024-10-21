385a8676bf3373205e420e19bae9d01b50e19635
Manuel Braun <manuel.braun.coder@gmail.com>
2024-10-21 08:30:24 +0200

    references: add embla carousel

--- src/app/components/references/references.component.ts
+++ src/app/components/references/references.component.ts
@@ -1,17 +1,73 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ReferenceComponent } from "../reference/reference.component";
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ReferenceComponent } from '../reference/reference.component';
+import {
+  EmblaCarouselDirective,
+  EmblaCarouselType,
+  EmblaEventType,
+} from 'embla-carousel-angular';
+import Autoplay from 'embla-carousel-autoplay';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [ReferenceComponent],
+  imports: [ReferenceComponent, EmblaCarouselDirective, CommonModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
-export class ReferencesComponent {
+export class ReferencesComponent implements AfterViewInit {
+  @ViewChild(EmblaCarouselDirective) emblaRef!: EmblaCarouselDirective;
 
+  private emblaApi?: EmblaCarouselType;
+  public options = { loop: true };
+  public plugins = [Autoplay()];
+  public activeSlide: number | undefined = undefined;
 
-  
+  ngAfterViewInit(): void {
+    this.emblaApi = this.emblaRef.emblaApi;
+    if (this.emblaApi) {
+      this.emblaApi.on('settle', () => {
+        this.activeSlide = this.emblaApi?.selectedScrollSnap();        
+      });
+    }
+    setTimeout(() => {
+      if (this.emblaApi) {
+        this.emblaApi.on('settle', () => {
+          this.emblaApi?.plugins().autoplay.play();
+        });
+      }
+    }, 4000);
+  }
 
+  readonly subscribeToEvents: EmblaEventType[] = [
+    'init',
+    'pointerDown',
+    'pointerUp',
+    'slidesChanged',
+    'slidesInView',
+    'select',
+    'settle',
+    'destroy',
+    'reInit',
+    'resize',
+    'scroll',
+  ];
 
+  onEmblaChanged(event: EmblaEventType): void {
+    // console.log(`Embla event triggered: ${event}`)
+  }
+
+  next() {
+    this.emblaApi?.scrollNext();
+  }
+
+  prev() {
+    this.emblaApi?.scrollPrev();
+  }
 }
