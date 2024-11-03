a2d316f57f4cd3f970a071cdd33002d2a5be4da9
Manuel Braun <manuel.braun.coder@gmail.com>
2024-11-03 08:37:09 +0100

    add popup for send mail confirmation

---
+++ src/app/components/confirm-popup/confirm-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-popup.component.html',
+  styleUrl: './confirm-popup.component.scss'
+})
+export class ConfirmPopupComponent {
+
+}
--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,29 +1,76 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { fadeIn } from "../../shared/animations";
+import { ConfirmPopupComponent } from "../confirm-popup/confirm-popup.component";
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  animations: [fadeIn],
+  imports: [CommonModule, FormsModule, RouterLink, ConfirmPopupComponent],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
+  private http = inject(HttpClient);
+  mailtest = false;
+  endPoint = 'https://beta-test.manuel-braun.net/sendMail.php';
+  showConfirmPopup = false;
 
   contact = {
     name: '',
     email: '',
     message: '',
-    acceptPrivacy: false
-  }
+    acceptPrivacy: false,
+  };
 
   onSubmit(ngForm: NgForm) {
-    console.log(this.contact); 
+    if (ngForm.valid && ngForm.submitted && !this.mailtest) {
+      console.log(this.contact);
+      this.handleSendMail(ngForm);
+    } else if (ngForm.valid && ngForm.submitted && this.mailtest) {
+      console.log(this.contact);
+      this.confirm();
+      ngForm.resetForm();
+    }
   }
 
-  toggleCheckbox(){
-    this.contact.acceptPrivacy = !this.contact.acceptPrivacy;    
+  toggleCheckbox() {
+    this.contact.acceptPrivacy = !this.contact.acceptPrivacy;
+  }
+
+  post = {
+    endPoint: this.endPoint,
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  handleSendMail(ngForm: NgForm) {
+    this.http.post(this.post.endPoint, this.post.body(this.contact)).subscribe({
+      next: (response) => {
+        ngForm.resetForm();
+      },
+      error: (error) => {
+        console.error('Error sending mail:', error);
+      },
+      complete: () => {
+        this.confirm();
+      },
+    });
+  }
+
+  confirm() {
+    this.showConfirmPopup = true;
+    setTimeout(() => {
+      this.showConfirmPopup = false;
+    }, 3500);
   }
 }
