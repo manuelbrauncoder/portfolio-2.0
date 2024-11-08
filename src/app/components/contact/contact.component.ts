eaf62ee7a1cc9aaed5d9cef8f7950a1cf4a4dd9f
Manuel Braun <manuel.braun.coder@gmail.com>
2024-11-08 22:32:49 +0100

    translate contact-form, start with contact translations

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ContactFormComponent } from "../contact-form/contact-form.component";
+import { TranslateModule } from '@ngx-translate/core';
+import { NgxTranslateService } from '../../services/ngx-translate.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ContactFormComponent],
+  imports: [ContactFormComponent, TranslateModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  private ngxService = inject(NgxTranslateService);
+
+  constructor(){
+    this.ngxService.initNgxTranslate();
+  }
 
 }
