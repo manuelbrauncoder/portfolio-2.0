3a90b96b7835fe6cfdc318f12b088d2d0d96bb0f
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-04 22:47:07 +0200

    Add error and success message in de and en for popup

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -2,18 +2,18 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { fadeIn } from "../../shared/animations";
-import { ConfirmPopupComponent } from "../confirm-popup/confirm-popup.component";
+import { fadeIn } from '../../shared/animations';
+import { ConfirmPopupComponent } from '../confirm-popup/confirm-popup.component';
 import { NgxTranslateService } from '../../services/ngx-translate.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { UiService } from '../../services/ui.service';
 
 @Component({
-    selector: 'app-contact-form',
-    animations: [fadeIn],
-    imports: [CommonModule, FormsModule, ConfirmPopupComponent, TranslateModule],
-    templateUrl: './contact-form.component.html',
-    styleUrl: './contact-form.component.scss'
+  selector: 'app-contact-form',
+  animations: [fadeIn],
+  imports: [CommonModule, FormsModule, ConfirmPopupComponent, TranslateModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
   private http = inject(HttpClient);
@@ -23,6 +23,15 @@ export class ContactFormComponent {
   mailtest = true;
   endPoint = 'https://beta-test.manuel-braun.net/sendMail.php';
   showConfirmPopup = false;
+  popupMessage = '';
+
+  err_de = 'Oops, da ist etwas schief gelaufen.';
+  err_en = 'Sorry, something went wrong.';
+
+  success_de =
+    'Danke für deine Nachricht. Ich werde mich so schnell wie möglich melden.';
+  success_en =
+    'Thanks for your message, i will contact you as soon as possible.';
 
   contact = {
     name: '',
@@ -35,12 +44,18 @@ export class ContactFormComponent {
     this.ngxService.initNgxTranslate();
   }
 
+  /**
+   * Send the email if form is valid and mailtest is set to false
+   *
+   * @param ngForm
+   */
   onSubmit(ngForm: NgForm) {
     if (ngForm.valid && ngForm.submitted && !this.mailtest) {
       console.log(this.contact);
-      this.handleSendMail(ngForm)
+      this.handleSendMail(ngForm);
     } else if (ngForm.valid && ngForm.submitted && this.mailtest) {
       console.log(this.contact);
+      this.setMessage(true);
       this.confirm();
       ngForm.resetForm();
     }
@@ -50,6 +65,9 @@ export class ContactFormComponent {
     this.contact.acceptPrivacy = !this.contact.acceptPrivacy;
   }
 
+  /**
+   * Defines the post method
+   */
   post = {
     endPoint: this.endPoint,
     body: (payload: any) => JSON.stringify(payload),
@@ -61,6 +79,12 @@ export class ContactFormComponent {
     },
   };
 
+  /**
+   * Send a Post Request to the php endpoint and
+   * handle Error and Confirmation
+   *
+   * @param ngForm
+   */
   handleSendMail(ngForm: NgForm) {
     this.http.post(this.post.endPoint, this.post.body(this.contact)).subscribe({
       next: (response) => {
@@ -68,17 +92,37 @@ export class ContactFormComponent {
       },
       error: (error) => {
         console.error('Error sending mail:', error);
+        this.setMessage(false);
+        this.confirm();
       },
       complete: () => {
+        this.setMessage(true)
         this.confirm();
       },
     });
   }
 
+  /**
+   * show the popup and hides it after timeout
+   */
   confirm() {
     this.showConfirmPopup = true;
     setTimeout(() => {
       this.showConfirmPopup = false;
     }, 3500);
+  }
+
+  /**
+   * Set the confirmation or error message
+   * in the choosen language
+   * @param success
+   */
+  setMessage(success: boolean) {
+    const lang = this.ngxService.getCurrentLanguage();
+    if (lang === 'en') {
+      this.popupMessage = success ? this.success_en : this.err_en;
+    } else {
+      this.popupMessage = success ? this.success_de : this.err_de;
+    }
   }
 }
